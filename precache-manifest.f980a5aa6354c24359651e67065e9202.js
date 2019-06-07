7ddd99808e46bc073a21a3d46c4cf055b59447e1
Brent Velthoen <brent.velthoen@gmail.com>
2019-06-07 12:11:10 -0700

    Updates

---
+++ precache-manifest.f980a5aa6354c24359651e67065e9202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48619e862061774a82110de098e1d1a0",
+    "url": "/sliding-tiles-puzzle/index.html"
+  },
+  {
+    "revision": "09cda1a99dcdb9004ca4",
+    "url": "/sliding-tiles-puzzle/static/css/main.1d101630.chunk.css"
+  },
+  {
+    "revision": "b1b5edc5543e007ed144",
+    "url": "/sliding-tiles-puzzle/static/js/2.60e0bc6e.chunk.js"
+  },
+  {
+    "revision": "09cda1a99dcdb9004ca4",
+    "url": "/sliding-tiles-puzzle/static/js/main.d2b6a60c.chunk.js"
+  },
+  {
+    "revision": "e6c6fbc0aa0672010bfb",
+    "url": "/sliding-tiles-puzzle/static/js/runtime~main.7ff0f980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sliding-tiles-puzzle/static/media/logo.5d5d9eef.svg"
+  }
+]);
